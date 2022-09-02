ac6b10d14d1c0fe8a6d5bca0be4dfcb35f945c1c
Seth Wright <seth.wright@willowtreeapps.com>
2022-09-02 14:30:33 -0400

    Remove some text.

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,8 +12,7 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
+        Try updating this message in the ./routes/index.tsx file, and refresh.
       </p>
       <Counter start={3} />
     </div>
